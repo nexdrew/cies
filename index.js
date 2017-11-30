3b17cb2efcfacd6091667ee1a7fced0163b6c049
nexdrew <andrewbgoode@gmail.com>
2017-11-29 22:41:06 -0500

    feat: use -v to lookup installed versions
    more info, prettier output
    
    BREAKING CHANGE: old -v flag is new -V flag, module returns array of objects instead of array of strings

--- index.js
+++ index.js
@@ -3,38 +3,62 @@
 const fs = require('fs')
 const path = require('path')
 
-function add (set, array) {
+function readPackageJson (dir, ignoreNotFound) {
+  const file = path.join(dir, 'package.json')
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+      if (err) {
+        if (err.code === 'ENOENT') {
+          if (ignoreNotFound) return resolve()
+          err = `File ${file} does not exist :(`
+        }
+        return reject(err)
+      }
+
+      try {
+        resolve(JSON.parse(data))
+      } catch (e) {
+        reject(e)
+      }
+    })
+  })
+}
+
+function add (deps, array, type) {
+  let dep
   for (let i = 0, len = array.length; i < len; i++) {
-    set.add(array[i])
+    dep = deps.find(d => d.name === array[i])
+    if (dep) dep.types.push(type)
+    else deps.push({ types: [type], name: array[i] })
   }
 }
 
 module.exports = function cies (opts) {
   opts = opts || {}
-  const file = path.join(opts.dir || process.cwd(), 'package.json')
-  return new Promise((resolve, reject) => {
-    fs.readFile(file, 'utf8', (err, data) => {
-      if (err) {
-        if (err.code === 'ENOENT') err = `File ${file} does not exist :(`
-        return reject(err)
-      }
+  const baseDir = opts.dir || process.cwd()
+  return readPackageJson(baseDir).then(pkg => {
+    const deps = []
+    if (pkg.dependencies && (opts.all || !opts.exclusive)) add(deps, Object.keys(pkg.dependencies), 'prod')
+    if (pkg.devDependencies && (opts.all || opts.dev)) add(deps, Object.keys(pkg.devDependencies), 'dev')
+    if (pkg.peerDependencies && (opts.all || opts.peer)) add(deps, Object.keys(pkg.peerDependencies), 'peer')
+    if ((pkg.bundledDependencies || pkg.bundleDependencies) && (opts.all || opts.bundled)) add(deps, pkg.bundledDependencies || pkg.bundleDependencies, 'bundled')
+    if (pkg.optionalDependencies && (opts.all || opts.optional)) add(deps, Object.keys(pkg.optionalDependencies), 'optional')
+    if (opts.sort) deps.sort((a, b) => a.name.localeCompare(b.name))
+    return deps
+  }).then(deps => {
+    if (opts.terse || !opts.versions) return deps
 
-      let pkg
-      try {
-        pkg = JSON.parse(data)
-      } catch (e) {
-        return reject(e)
+    const promises = []
+    let scopeName
+    deps.forEach(d => {
+      scopeName = d.name.split('/')
+      promises.push(readPackageJson(path.join(baseDir, 'node_modules', scopeName[0], scopeName[1] || ''), true))
+    })
+    return Promise.all(promises).then(pkgs => {
+      for (let i = 0, len = pkgs.length; i < len; i++) {
+        deps[i].version = (pkgs[i] && pkgs[i].version) || 'not installed'
       }
-
-      const deps = new Set()
-      if (pkg.dependencies && (opts.all || !opts.exclusive)) add(deps, Object.keys(pkg.dependencies))
-      if (pkg.devDependencies && (opts.all || opts.dev)) add(deps, Object.keys(pkg.devDependencies))
-      if (pkg.peerDependencies && (opts.all || opts.peer)) add(deps, Object.keys(pkg.peerDependencies))
-      if ((pkg.bundledDependencies || pkg.bundleDependencies) && (opts.all || opts.bundled)) add(deps, pkg.bundledDependencies || pkg.bundleDependencies)
-      if (pkg.optionalDependencies && (opts.all || opts.optional)) add(deps, Object.keys(pkg.optionalDependencies))
-      const array = Array.from(deps)
-      if (opts.sort) array.sort()
-      resolve(array)
+      return deps
     })
   })
 }
--- test/test-cli.js
+++ test/test-cli.js
@@ -1,15 +1,23 @@
 'use strict'
 
+process.env.FORCE_COLOR = '1'
+
 const test = require('tap').test
 const cp = require('child_process')
 const path = require('path')
+const chalk = require('chalk')
 
 const parentPath = path.resolve(__dirname, '..')
 const cliPath = path.join(parentPath, 'cli.js')
 
-function cli (args) {
+function cli (args, disableColor) {
+  const env = Object.assign({}, process.env)
+  if (disableColor) delete env.FORCE_COLOR
   return new Promise((resolve, reject) => {
-    cp.execFile(cliPath, args ? args.split(/\s/) : [], { encoding: 'utf8' }, (err, stdout, stderr) => {
+    cp.execFile(cliPath, args ? args.split(/\s/) : [], {
+      encoding: 'utf8',
+      env
+    }, (err, stdout, stderr) => {
       if (err) return reject(err)
       resolve({ stdout, stderr })
     })
@@ -22,28 +30,66 @@ function withLineFeeds (array) {
 
 test('default includes dependencies', t => {
   return cli().then(r => {
-    t.equal(r.stdout, withLineFeeds(['sywac']))
+    t.equal(r.stdout, withLineFeeds([
+      chalk`{white chalk} {cyan prod}`,
+      chalk`{white sywac} {cyan prod}`
+    ]))
     t.notOk(r.stderr)
   })
 })
 
 test('--dev includes dependencies and devDependencies', t => {
   return cli(`${parentPath} --dev`).then(r => {
-    t.equal(r.stdout, withLineFeeds(['sywac', 'coveralls', 'standard', 'standard-version', 'tap']))
+    t.equal(r.stdout, withLineFeeds([
+      chalk`{white chalk}            {cyan prod}`,
+      chalk`{white sywac}            {cyan prod}`,
+      chalk`{white coveralls}        {magenta dev}`,
+      chalk`{white standard}         {magenta dev}`,
+      chalk`{white standard-version} {magenta dev}`,
+      chalk`{white tap}              {magenta dev}`
+    ]))
     t.notOk(r.stderr)
   })
 })
 
 test('--all includes everything defined', t => {
   return cli(`${parentPath} --all`).then(r => {
-    t.equal(r.stdout, withLineFeeds(['sywac', 'coveralls', 'standard', 'standard-version', 'tap']))
+    t.equal(r.stdout, withLineFeeds([
+      chalk`{white chalk}            {cyan prod}`,
+      chalk`{white sywac}            {cyan prod}`,
+      chalk`{white coveralls}        {magenta dev}`,
+      chalk`{white standard}         {magenta dev}`,
+      chalk`{white standard-version} {magenta dev}`,
+      chalk`{white tap}              {magenta dev}`
+    ]))
     t.notOk(r.stderr)
   })
 })
 
 test('--sort sorts list', t => {
   return cli(`${parentPath} --all --sort`).then(r => {
-    t.equal(r.stdout, withLineFeeds(['coveralls', 'standard', 'standard-version', 'sywac', 'tap']))
+    t.equal(r.stdout, withLineFeeds([
+      chalk`{white chalk}            {cyan prod}`,
+      chalk`{white coveralls}        {magenta dev}`,
+      chalk`{white standard}         {magenta dev}`,
+      chalk`{white standard-version} {magenta dev}`,
+      chalk`{white sywac}            {cyan prod}`,
+      chalk`{white tap}              {magenta dev}`
+    ]))
+    t.notOk(r.stderr)
+  })
+})
+
+test('--terse prints uncolored names only', t => {
+  return cli(`${parentPath} --all --terse`).then(r => {
+    t.equal(r.stdout, withLineFeeds(['chalk', 'sywac', 'coveralls', 'standard', 'standard-version', 'tap']))
+    t.notOk(r.stderr)
+  })
+})
+
+test('infers --terse if no color supported (e.g. when piping stdout to another program)', t => {
+  return cli(`${parentPath} -a`, true).then(r => {
+    t.equal(r.stdout, withLineFeeds(['chalk', 'sywac', 'coveralls', 'standard', 'standard-version', 'tap']))
     t.notOk(r.stderr)
   })
 })
@@ -62,6 +108,13 @@ test('fails if package.json invalid json', t => {
   })
 })
 
+test('fails if package.json is a directory ¯\\_(ツ)_/¯', t => {
+  return cli(path.resolve(__dirname)).catch(err => {
+    t.equal(err.code, 1)
+    t.includes(err.message, 'EISDIR')
+  })
+})
+
 test('prints nothing if no dependencies', t => {
   return cli(path.resolve(__dirname, 'empty')).then(r => {
     t.equal(r.stdout, '')
@@ -71,14 +124,56 @@ test('prints nothing if no dependencies', t => {
 
 test('supports all dependency types', t => {
   return cli(`${path.resolve(__dirname, 'all')} -dpbos`).then(r => {
-    t.equal(r.stdout, withLineFeeds(['chai', 'chalk', 'cliui', 'decamelize', 'dezalgo', 'find-up', 'fs-vacuum', 'react', 'react-dom', 'react-router', 'redux-simple-router', 'which', 'write-file-atomic']))
+    t.equal(r.stdout, withLineFeeds([
+      chalk`{white @nexdrew/slugid}     {cyan prod}`,
+      chalk`{white chai}                {magenta dev}`,
+      chalk`{white chalk}               {magenta dev}`,
+      chalk`{white cliui}               {cyan prod},{green bundled}`,
+      chalk`{white decamelize}          {cyan prod}`,
+      chalk`{white dezalgo}             {green bundled}`,
+      chalk`{white find-up}             {cyan prod}`,
+      chalk`{white fs-vacuum}           {green bundled}`,
+      chalk`{white react}               {blue peer}`,
+      chalk`{white react-dom}           {blue peer}`,
+      chalk`{white react-router}        {yellow optional}`,
+      chalk`{white redux-simple-router} {yellow optional}`,
+      chalk`{white which}               {magenta dev}`,
+      chalk`{white write-file-atomic}   {green bundled}`
+    ]))
+    t.notOk(r.stderr)
+  })
+})
+
+test('-v looks up installed versions', t => {
+  return cli(`${path.resolve(__dirname, 'all')} -asv`).then(r => {
+    t.equal(r.stdout, withLineFeeds([
+      chalk`{white @nexdrew/slugid}     {cyan prod}         {inverse 2.0.1}`,
+      chalk`{white chai}                {magenta dev}          {inverse not installed}`,
+      chalk`{white chalk}               {magenta dev}          {inverse not installed}`,
+      chalk`{white cliui}               {cyan prod},{green bundled} {inverse 3.3.0}`,
+      chalk`{white decamelize}          {cyan prod}         {inverse 1.1.1}`,
+      chalk`{white dezalgo}             {green bundled}      {inverse not installed}`,
+      chalk`{white find-up}             {cyan prod}         {inverse 2.1.0}`,
+      chalk`{white fs-vacuum}           {green bundled}      {inverse not installed}`,
+      chalk`{white react}               {blue peer}         {inverse not installed}`,
+      chalk`{white react-dom}           {blue peer}         {inverse not installed}`,
+      chalk`{white react-router}        {yellow optional}     {inverse not installed}`,
+      chalk`{white redux-simple-router} {yellow optional}     {inverse not installed}`,
+      chalk`{white which}               {magenta dev}          {inverse not installed}`,
+      chalk`{white write-file-atomic}   {green bundled}      {inverse not installed}`
+    ]))
     t.notOk(r.stderr)
   })
 })
 
 test('supports alt bundleDependencies', t => {
   return cli(`${path.resolve(__dirname, 'bundle-alt')} -xb`).then(r => {
-    t.equal(r.stdout, withLineFeeds(['abbrev', 'ansi-regex', 'ansicolors', 'aproba']))
+    t.equal(r.stdout, withLineFeeds([
+      chalk`{white abbrev}     {green bundled}`,
+      chalk`{white ansi-regex} {green bundled}`,
+      chalk`{white ansicolors} {green bundled}`,
+      chalk`{white aproba}     {green bundled}`
+    ]))
     t.notOk(r.stderr)
   })
 })
